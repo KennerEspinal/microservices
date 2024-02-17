35b2c2924b75051f0cebf0531dfebf388dc627cc
KennerEspinal <kennerespinalg@gmail.com>
2024-02-17 17:00:15 -0500

    first commit

---
+++ products-services/src/test/java/com/kendev/products_services/ProductsServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kendev.products_services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
